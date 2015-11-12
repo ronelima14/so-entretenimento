92f63894de501a96210ab47e9554334383b34cc6
Matheus Martins <Casa@iMac-de-Matheus-Martins.local>
2015-11-12 11:13:29 -0200

    fixing identity

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
         <!-- side main -->
         <section>
             <img src="http://migre.me/s5Ugq" alt="" class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-            <img src="http://migre.me/s5Ugq " alt="" class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+            <img src="http://migre.me/s5Ugq" alt="" class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
         </section>
         <div class="clearfix"></div>
         <!-- categories -->
