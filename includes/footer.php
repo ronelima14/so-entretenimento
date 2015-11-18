413cd3541f048141497da0fee39043972a089f00
Matheus Martins <matheus@matheuswd.com>
2015-11-18 13:33:32 -0200

    fixing footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
         <footer>
-	         <nav class="navbar navbar-default">
+	         <nav class="navbar navbar-default footer-bottom">
 	            <div class="container">
 	            	<ul class="nav navbar-nav">
 		                <li><a href="index.php">Home</a></li>
