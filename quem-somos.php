a17060ac052e33d6f4c1601ffb2bba0aa1e098b3
matheuswd <matheus@matheuswd.com>
2015-12-04 13:05:22 -0200

    finishing 'quem somos' page

--- quem-somos.php
+++ quem-somos.php
@@ -16,32 +16,96 @@
     <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
+    <div class="bg-qs">
     
     	<?php
     		@include "includes/header.php";
     	?>
     <div class="container">
         <section>
+            <div class="container qs-border text-center">
                 <h1>Quem somos</h1>
-                <div class="panel panel-default">
-                  <div class="panel-heading">
-                    <h3 class="panel-title">Matheus de Almeida Martins</h3>
-                  </div>
-                  <div class="panel-body">
-                    <img src="images/musica-matheus.jpg" alt="Imagem de Matheus de Almeida Martins, gerente do projeto" class="col-xs-6 col-sm-4 col-md-3 col-lg-3 img-responsive img-circle personal">
-                    <p class=""><strong>Mini biografia:</strong></p>
-                    <p>Matheus é um cara muito tranquilo que gosta muito das aulas de PPSI na faculdade.</p>
-                    <p class=""><strong>Responsabilidade:</strong></p>
-                    <p>Gerente de projeto</p>
-                    <a href="http://github.com/matheuswd">
-                        <p class="btn btn-default"><img src="images/icone-github.png" alt="Ícone do Github" class="icons"> Github</p>
-                    </a>
-                  </div>
-                </div>
+            </div>
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h3 class="panel-title">Matheus de Almeida Martins</h3>
+              </div>
+              <div class="panel-body">
+                <img src="images/musica-matheus.jpg" alt="Imagem de Matheus de Almeida Martins, gerente do projeto" class="col-xs-6 col-sm-4 col-md-3 col-lg-3 img-responsive img-circle personal">
+                <p class=""><strong>Mini biografia:</strong></p>
+                <p>Matheus é um cara muito tranquilo que gosta muito das aulas de PPSI na faculdade.</p>
+                <p class=""><strong>Responsabilidade:</strong></p>
+                <p>Gerente de projeto</p>
+                <a href="https://github.com/matheuswd">
+                    <p class="btn btn-default"><img src="images/icone-github.png" alt="Ícone do Github" class="icons"> Github</p>
+                </a>
+              </div>
+            </div>
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h3 class="panel-title">Jonadab Ferreira da Silva</h3>
+              </div>
+              <div class="panel-body">
+                <img src="images/jonadab.jpg" alt="Imagem de Matheus de Almeida Martins, gerente do projeto" class="col-xs-6 col-sm-4 col-md-3 col-lg-3 img-responsive img-circle personal">
+                <p class=""><strong>Mini biografia:</strong></p>
+                <p>Um cara que curte uma cerveja, um truco e odeia jogar na equipe do Matheus porque ele é muito chato.</p>
+                <p class=""><strong>Responsabilidade:</strong></p>
+                <p>Sessão de Filmes</p>
+                <a href="https://github.com/Jonadab01">
+                    <p class="btn btn-default"><img src="images/icone-github.png" alt="Ícone do Github" class="icons"> Github</p>
+                </a>
+              </div>
+            </div>
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h3 class="panel-title">Matheus de Almeida Martins</h3>
+              </div>
+              <div class="panel-body">
+                <img src="images/musica-matheus.jpg" alt="Imagem de Matheus de Almeida Martins, gerente do projeto" class="col-xs-6 col-sm-4 col-md-3 col-lg-3 img-responsive img-circle personal">
+                <p class=""><strong>Mini biografia:</strong></p>
+                <p>Matheus é um cara muito tranquilo que gosta muito das aulas de PPSI na faculdade.</p>
+                <p class=""><strong>Responsabilidade:</strong></p>
+                <p>Gerente de projeto</p>
+                <a href="http://github.com/matheuswd">
+                    <p class="btn btn-default"><img src="images/icone-github.png" alt="Ícone do Github" class="icons"> Github</p>
+                </a>
+              </div>
+            </div>
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h3 class="panel-title">Matheus de Almeida Martins</h3>
+              </div>
+              <div class="panel-body">
+                <img src="images/musica-matheus.jpg" alt="Imagem de Matheus de Almeida Martins, gerente do projeto" class="col-xs-6 col-sm-4 col-md-3 col-lg-3 img-responsive img-circle personal">
+                <p class=""><strong>Mini biografia:</strong></p>
+                <p>Matheus é um cara muito tranquilo que gosta muito das aulas de PPSI na faculdade.</p>
+                <p class=""><strong>Responsabilidade:</strong></p>
+                <p>Gerente de projeto</p>
+                <a href="http://github.com/matheuswd">
+                    <p class="btn btn-default"><img src="images/icone-github.png" alt="Ícone do Github" class="icons"> Github</p>
+                </a>
+              </div>
+            </div>
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h3 class="panel-title">Matheus de Almeida Martins</h3>
+              </div>
+              <div class="panel-body">
+                <img src="images/musica-matheus.jpg" alt="Imagem de Matheus de Almeida Martins, gerente do projeto" class="col-xs-6 col-sm-4 col-md-3 col-lg-3 img-responsive img-circle personal">
+                <p class=""><strong>Mini biografia:</strong></p>
+                <p>Matheus é um cara muito tranquilo que gosta muito das aulas de PPSI na faculdade.</p>
+                <p class=""><strong>Responsabilidade:</strong></p>
+                <p>Gerente de projeto</p>
+                <a href="http://github.com/matheuswd">
+                    <p class="btn btn-default"><img src="images/icone-github.png" alt="Ícone do Github" class="icons"> Github</p>
+                </a>
+              </div>
+            </div>
         </section>
     </div>
 <?php
     @include "includes/footer.php";
 ?>
+    </div>
 </body>
 </html>
