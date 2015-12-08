9b636b2d6c56bf44a9c4eb44700f67b6505925e7
matheuswd <matheus@matheuswd.com>
2015-12-08 14:43:06 -0200

    adding h1s section

--- hqs.php
+++ hqs.php
@@ -24,52 +24,63 @@
     <div class="container">
         <!-- main -->
         <article class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
-        <section>
-            <h5>DC X MARVEL</h5>
-            <a href="padrao-noticia.php"><img src="images/l1.jpg" alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-4"></a>
-            <a href="padrao-noticia.php"><img src="images/l2.jpg" alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-4"></a>
-            <a href="padrao-noticia.php"><img src="images/destaque.jpg" alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-4"></a>
             
-       </section>
+        <section>
+            <h5 class="text-center hq-texto">DC X MARVEL</h5>
+            <a href="hq-gibi1.php"><img src="images/hq-capa3.jpg"  alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-3"></a>
+            <a href="padrao-noticia.php"><img src="images/hq-capa2.jpg"  alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-3"></a>
+            <a href="padrao-noticia.php"><img src="images/hq-capa1.jpg" alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-3"></a>
+            <a href="padrao-noticia.php"><img src="images/hq-capa4.jpg"  alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-3"></a>
+        </section>
       
+            
         <section>
-            <h5>VARIADOS</h5>
-            <a href="padrao-noticia.php"><img src="images/l2.jpg" alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-4"></a>
-            <a href="padrao-noticia.php"><img src="images/destaque.jpg" alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-4"></a>
-            <a href="padrao-noticia.php"><img src="images/l1.jpg" alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-4"></a>
+            <h5 class="text-center hq-texto">VARIADOS</h5>
+            <a href="padrao-noticia.php"><img src="images/hq-capa8.jpg"  alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-3"></a>
+            <a href="padrao-noticia.php"><img src="images/hq-capa6.jpg"  alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-3"></a>
+            <a href="padrao-noticia.php"><img src="images/hq-capa7.jpg"  alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-3"></a>
+            <a href="padrao-noticia.php"><img src="images/hq-capa5.jpg"  alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-3"></a>
         </section>
         
+           
         <section>
-            <h5>MANGÁS</h5>
-            <a href="padrao-noticia.php"><img src="images/destaque.jpg" alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-4"></a>
-            <a href="padrao-noticia.php"><img src="images/l1.jpg" alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-4"></a>
-            <a href="padrao-noticia.php"><img src="images/l2.jpg" alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-4"></a>
+            <h5 class="text-center hq-texto">MANGÁS</h5>
+            <a href="padrao-noticia.php"><img src="images/hq-capa12.jpg"  alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-3"></a>
+            <a href="padrao-noticia.php"><img src="images/hq-capa10.jpg"  alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-3"></a>
+            <a href="padrao-noticia.php"><img src="images/hq-capa11.jpg"  alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-3"></a>
+            <a href="padrao-noticia.php"><img src="images/hq-capa9.jpg"  alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-3"></a>
         </section>
         
+            
         <section>
-            <h5>EVENTOS</h5>
-            <a href="padrao-noticia.php"><img src="images/l1.jpg" alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-4"></a>
-            <a href="padrao-noticia.php"><img src="images/l2.jpg" alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-4"></a>
-            <a href="padrao-noticia.php"><img src="images/destaque.jpg" alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-4"></a>
+            <h5 class="text-center hq-texto">EVENTOS</h5>
+            <a href="padrao-noticia.php"><img src="images/hq-evento1.jpg"  alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-4"></a>
+            <a href="padrao-noticia.php"><img src="images/hq-evento2.jpg"  alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-4"></a>
+            <a href="padrao-noticia.php"><img src="images/hq-evento3.jpg"  alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-3 col-lg-4"></a>
         </section>
         
         </article>
 
         <aside class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-            <h2>DESTAQUE DO DIA!</h2>
-                 <a href="http://localhost/integrador/padrao-noticia.php"><img src="images/destaque2.jpeg" alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-12 col-lg-12"></a>
+            <h2 class="text-center hq-tittlea">DESTAQUE DO DIA!</h2>
+                 <a href="http://localhost/integrador/padrao-noticia.php"><img src="images/hq-destaque.jpg" alt="imagem de teste" class="col-xs-12 col-sm-12 col-md-12 col-lg-12"></a>
                  <section class="col-xs-12 col-sm-12 col-md-4 col-lg-12">
-                   <h3>Guerra Civil</h3>
-                   <h4>Moore, Stuart</h4>
+                   <h3 class="hq-tittleb">Guerra Civil</h3>
+                   <h4 class="hq-tittlec">Moore, Stuart</h4>
                    <h4>Sobre o Produto</h4>
-                   <h5>A épica história que provoca a separação do Universo Marvel. Homem de Ferro e Capitão América: dois membros essenciais para os Vingadores, a maior equipe de super-heróis do mundo. Quando uma trágica batalha deixa um buraco na cidade de Stamford, matando centenas de pessoas, o governo americano exige que todos os super-heróis revelem sua identidade e registrem seus poderes. Para Tony Stark - o Homem de Ferro - é um passo lamentável, porém necessário, o que o leva a apoiar a lei. Para o Capitão América, é uma intolerável agressão à liberdade cívica. Assim começa a 'Guerra Civil'.A épica história que provoca a separação do Universo Marvel. Homem de Ferro e Capitão América: dois membros essenciais para os Vingadores, a maior equipe de super-heróis do mundo. Quando uma trágica batalha deixa um buraco na cidade de Stamford, matando centenas de pessoas, o governo americano exige que todos os super-heróis revelem sua identidade e registrem seus poderes. Para Tony Stark - o Homem de Ferro - é um passo lamentável, porém necessário, o que o leva a apoiar a lei. Para o Capitão América, é uma intolerável agressão à liberdade cívica. Assim começa a 'Guerra Civil'.</h5>
+                   <p>A épica história que provoca a separação do Universo Marvel. 
+                   Homem de Ferro e Capitão América: dois membros essenciais para os Vingadores, 
+                   a maior equipe de super-heróis do mundo. Quando uma trágica batalha deixa um buraco na cidade de Stamford, 
+                   atando centenas de pessoas, o governo americano exige que todos os super-heróis revelem sua identidade
+                    e registrem seus poderes...</p>
+                    <button class="btn btn-default" type="submit">Veja Mais</button>
                  </section>
         </aside>
-        <section class="col-xs-10 col-sm-6 col-md-8 col-lg-8"><img src="images/propaganda.jpg" alt="" class="col-xs-10 col-sm-12 col-md-12 col-lg-12"></section>
-    </div>
+</div>
 <?php
     @include "includes/footer.php";
     @include "includes/scripts.php";
 ?>
+
 </body>
 </html>
